bd21e2f4a4de557a8a50da72c73e03a4370bf254
KRAD <978404740@qq.com>
2019-05-24 15:29:00 +0800

    Change the COMP to two module

--- ALU_modelsource/COMP.v
+++ ALU_modelsource/COMP.v
@@ -1,15 +1,22 @@
-module COMP(out,A,B,less); //if less=1,out will be the less one,else if less=0,out is the grater one.
+module COMP_less(out,A,B,energy); //output the less one.
 input [15:0] A,B;
-input less;
+input energy;
 output [15:0] out;
-wire [15:0] outG,outL; //outG is the greater one,outL is the less one
 wire [15:0] sub;
 wire subn;
-ADD_SUB sub0(sub,,,A,B,1);
+ADD_SUB sub0(sub,,,A,B,energy);
+not(subn,sub[15]);
+two_mux_one16bit mux1(out,sub[15],A,B);
+endmodule
+
+module COMP_great(out,A,B,energy); //output the great one
+input [15:0] A,B;
+input energy;
+output [15:0] out;
+wire [15:0] sub;
+wire subn;
+ADD_SUB sub0(sub,,,A,B,energy);
 
 not(subn,sub[15]);
-
-two_mux_one16bit mux1(outL,sub[15],A,B);
-two_mux_one16bit mux2(outG,subn,A,B);
-two_mux_one16bit mux3(out,less,outL,outG);
+two_mux_one16bit mux2(out,subn,A,B);
 endmodule
